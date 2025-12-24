ab40da6bd61cd5545f61e179ce46a028fa05b3ea
Andrew Read <andrew81244@outlook.com>
2025-12-25 02:01:48 +0800

    update unit names

--- FiniteFractions/FiniteFractionsTypes.f
+++ FiniteFractions/FiniteFractionsTypes.f
@@ -1,10 +1,11 @@
 \ finite fraction data types that also serve for labeling in code and scripts
 
 ( hh mm ss - t) \ minutes and seconds of time
-: TIME ~ ;
-: RA   ~ ;	
+: HHMMSS ~ ;
+: RA ~ ;	
 
-( deg mm ss - t) \ minutes and seconds of arc
+( deg mm ss - t) \ minutes and seconds of arc  
+: DEGMINSEC ~ ;
 : DEC  ~ ; 
 : ALT  ~ ; 
 : AZ	 ~ ; 
