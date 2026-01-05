5db1ab1fdedb4961e544c912a627efe1690db32c
Andrew <andrew81244@outlook.com>
2026-01-05 09:03:14 +0800

    Add FITS$ function and enhance comments for convenience functions

--- FiniteFractions/FiniteFractionsTypes.f
+++ FiniteFractions/FiniteFractionsTypes.f
@@ -13,14 +13,20 @@
 : LONG ~ ;
 
 
-\ convenience functions for users
+\ convenience functions standard usage
+
+: ~FITS$ ( x-y-z -- caddr u)
+\ obtain an  string in the format XX YY ZZ from single integer finite fraction format, suitable for the FITS file header
+   BL BL 0 ~custom$
+ ;
 
 : .Dec ( DEGMMSS --)
+\ print a declination value to the console
     ':' ':' -1 ~custom$ type
  ;
 
 : .RA ( HHMMSS --)
- \ format for the :newalpt command
+\ print a time value (often RA) to the console
     ':' ':' 0 ~custom$ type
  ;
  
